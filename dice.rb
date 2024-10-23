c3dc722a59a900bb6bf75df45ee9bd14d5437a99
jenchan-938 <jchan5@uchicago.edu>
2024-10-23 04:21:29 +0000

    11:21

--- dice.rb
+++ dice.rb
@@ -1,10 +1,27 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/zebra") do
-  "We must add a route for each path we want to support"
+
+get ("/dice/2/6") do
+    first_die = rand(1..6)
+    second_die = rand(1..6)
+    sum = first_die + second_die
+
+    outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+    "<h1>2d6</h1>
+    <p>#{outcome}</p>"
 end
 
-get("/giraffe") do
-  "Hopefully this shows up without having to restart the server 🤞🏾"
+get ("/dice/2/20") do
+    first_die = rand(1..20)
+    second_die= rand(1..20)
+    sum = first_die + second_die
+
+    outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+
+    "<h1>2d20</h1>
+    <p>#{outcome}</p>"
+
+
 end
