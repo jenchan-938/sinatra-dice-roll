df867c41979439bd228ef34b93624b387319404c
jenchan-938 <jchan5@uchicago.edu>
2024-10-24 00:01:03 +0000

    7pm

--- dice.rb
+++ dice.rb
@@ -57,3 +57,14 @@ get ("/dice/5/4") do
     erb(:five_fours)
    
 end
+
+get ("/dice/100/6") do
+    @rolls = []
+
+    100.times do
+        die = rand(1..6)
+        @rolls.push(die)
+    end
+
+    erb(:one_hundred_six)
+end
